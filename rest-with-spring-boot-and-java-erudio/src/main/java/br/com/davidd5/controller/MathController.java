2c850321ec71f370cf4e4e64c79f55941a644e8a
Clovin <fudeoagr12@gmail.com>
2026-02-12 10:00:59 -0300

    definindo a logica da api RestFul

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/MathController.java
@@ -12,8 +12,17 @@ public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
            @PathVariable("numberOne") String numberOne,
-           @PathVariable("numberTwo") String numberTwo){
+           @PathVariable("numberTwo") String numberTwo)throws Exception{
 
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String numberOne) {
         return 1D;
+    }
+
+    private boolean isNumeric(String numberOne) {
+        return true;
     }
 }
