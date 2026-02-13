27054e2e54e446edb39a6df895fae145c3d38335
Clovin <fudeoagr12@gmail.com>
2026-02-13 08:49:21 -0300

    configurando os metodos isNumeric e convertDouble

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/MathController.java
@@ -18,11 +18,19 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
-    private Double convertToDouble(String numberOne) {
-        return 1D;
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strNumber.replace(", ", ".");
+
+        return Double.parseDouble(number);
     }
 
-    private boolean isNumeric(String numberOne) {
-        return true;
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) return false;
+
+        String number = strNumber.replace(", ", ".");  // trocar toda ocorrencia de virgula por ponto
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+
+        }
     }
-}
+
