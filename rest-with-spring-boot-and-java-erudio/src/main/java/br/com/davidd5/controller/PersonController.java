1fdd088acff30dadb194e1a76e8a2762ec9c8a2e
Clovin <fudeoagr12@gmail.com>
2026-02-19 11:10:07 -0300

    .\Refactoring do controller de Pessoas

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/PersonController.java
@@ -4,6 +4,7 @@ import br.com.davidd5.service.PersonService;
 import br.com.davidd5.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,15 +17,13 @@ public class PersonController {
     private PersonService service;
 
 
-    @RequestMapping(method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 
     public List<Person> findAll(){
         return service.findAll();
     }
 
-    @RequestMapping(value = "{id}",
-    method = RequestMethod.GET,
+    @GetMapping(value = "{id}",
     produces = MediaType.APPLICATION_JSON_VALUE)
 
     public Person findById(@PathVariable("id") Long id){
@@ -32,8 +31,7 @@ public class PersonController {
     }
 
 
-      @RequestMapping(
-    method = RequestMethod.POST,
+      @PostMapping(
     consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
 
@@ -41,8 +39,7 @@ public class PersonController {
           return service.create(person);
     }
 
-      @RequestMapping(
-    method = RequestMethod.PUT,
+      @PutMapping(
     consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
 
@@ -52,11 +49,10 @@ public class PersonController {
 
 
 
-    @RequestMapping(value = "{id}",
-            method = RequestMethod.DELETE)
-
-    public void delete(@PathVariable("id") Long id){
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
