0fde1d0d9bc2c09f212c3a0a8be79bc5ae0437d3
Clovin <fudeoagr12@gmail.com>
2026-03-02 08:54:57 -0300

    migrations com flyway

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/TestLogController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/davidd5/controller/TestLogController.java
@@ -4,10 +4,12 @@ import br.com.davidd5.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("api/test/v1")
 public class TestLogController {
 
     private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
